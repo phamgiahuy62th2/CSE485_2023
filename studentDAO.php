13e8147e6ba04a363a940f495e2caddda827790f
dinhquangduy2002 <99933117+dinhquangduy2002@users.noreply.github.com>
2023-05-10 22:56:58 +0700

    update

--- studentDAO.php
+++ studentDAO.php
@@ -1,50 +1,77 @@
 <?php
 class StudentDAO {
-    private $students;
+    private $students = [];
+
     public function __construct() {
-      $this->students = array();
+        $this->loadData();
     }
-  
-    // Create
-    public function addStudent($student) {
-      $this->students[] = $student;
-    }
-  
-    // Read
-    public function getStudentById($id) {
-      foreach ($this->students as $student) {
-        if ($student->getId() == $id) {
-          return $student;
+
+    private function loadData() {
+        if (($handle = fopen("students.csv", "r")) !== FALSE) {
+            while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                $id = $data[0];
+                $name = $data[1];
+                $age = $data[2];
+
+                $student = new Student($id, $name, $age);
+                array_push($this->students, $student);
+            }
+            fclose($handle);
         }
-      }
-      return null;
     }
-    // Update
-    public function updateStudent($id, $name, $age) {
-      foreach ($this->students as &$student) {
-        if ($student->getId() == $id) {
-          $student->setName($name);
-          $student->setAge($age);
-          return true;
-        }
-      }
-      return false;
-    }
-  
-    // Delete
-    public function deleteStudentById($id) {
-      foreach ($this->students as $index => $student) {
-        if ($student->getId() == $id) {
-          unset($this->students[$index]);
-          return true;
-        }
-      }
-      return false;
-    }
-    //getALL
-    public function getAllStudents() {
+
+    public function getAll() {
         return $this->students;
-      }
-  }
-  
+    }
+
+    public function getById($id) {
+        foreach ($this->students as $student) {
+            if ($student->getId() == $id) {
+                return $student;
+            }
+        }
+        return null;
+    }
+
+    public function add($student) {
+        if ($this->getById($student->getId()) == null) {
+            array_push($this->students, $student);
+            $this->saveData();
+            return true;
+        }
+        return false;
+    }
+
+    public function update($id, $student) {
+        foreach ($this->students as $key => $obj) {
+            if ($obj->getId() == $id) {
+                $this->students[$key] = $student;
+                $this->saveData();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function delete($id) {
+        foreach ($this->students as $key => $obj) {
+            if ($obj->getId() == $id) {
+                unset($this->students[$key]);
+                $this->students = array_values($this->students);
+                $this->saveData();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private function saveData() {
+        $handle = fopen("students.csv", "w");
+        foreach ($this->students as $student) {
+            fputcsv($handle, [$student->getId(), $student->getName(), $student->getAge()]);
+        }
+        fclose($handle);
+    }
+}
+
 ?>
