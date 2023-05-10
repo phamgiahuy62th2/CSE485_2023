b4d9c9e4ee25731312ef16cbd4316d77f8790b1c
dinhquangduy2002 <99933117+dinhquangduy2002@users.noreply.github.com>
2023-05-10 22:38:28 +0700

    upload

---
+++ Student.php
@@ -0,0 +1,38 @@
+<?php
+class Student {
+    private $id;
+    private $name;
+    private $age;
+
+    public function __construct($id, $name, $age) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function setAge($age) {
+        $this->age = $age;
+    }
+}
+
+?>
