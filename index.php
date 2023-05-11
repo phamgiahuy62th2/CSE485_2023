e3cc3bbe4dc14008bc1d263b451121fba1600510
your name <100056958+phamgiahuy62th2@users.noreply.github.com>
2023-05-11 13:43:46 +0700

    sửa code index.php

--- index.php
+++ index.php
@@ -16,6 +16,34 @@
 </head>
 <body>
     <?php
+    // Kiểm tra xem form đã được submit hay chưa
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Lấy dữ liệu từ form
+        $id = $_POST['id'];
+        $name = $_POST['name'];
+        $age = $_POST['age'];
+
+        // Kiểm tra sự trùng lặp
+        $isDuplicate = false;
+        $file = fopen("students.txt", "r");
+        if ($file) {
+            while (($line = fgets($file)) !== false) {
+                $data = explode("\t", $line);
+                if (trim($data[0]) == $id || trim($data[1]) == $name) {
+                    $isDuplicate = true;
+                    break;
+                }
+            }
+            fclose($file);
+        }
+
+        // Nếu không có sự trùng lặp, lưu dữ liệu vào file
+        if (!$isDuplicate) {
+            $newStudent = "$id\t$name\t$age\n";
+            file_put_contents("students.txt", $newStudent, FILE_APPEND);
+        }
+    }
+
     // Đọc dữ liệu từ file và lưu vào danh sách sinh viên
     $students = array();
     $file = fopen("students.txt", "r");
@@ -48,5 +76,18 @@
             </tr>
         <?php endforeach; ?>
     </table>
-</body>
-</html>
+
+    <h2>Thêm sinh viên mới</h2>
+    <form method="POST">
+        <label for="id">ID:</label>
+        <input type="text" name="id" required>
+        <br>
+        <label for="name">Họ và tên:</label>
+        <input type="text" name="name" required>
+        <br>
+        <label for="age">Tuổi:</label>
+        <input type="number" name="age" required>
+        <br>
+        <input type="submit" value="Thêm sinh viên">
+    </form>
+</body
